1982c29f1ad1f398707acceaa514a85742cf32bf
MichaelRomero <liikemike@aim.com>
2019-05-05 18:19:02 -0400

    added background colors and set layout for quiz-splash

--- src/components/quiz/QuizSplash.js
+++ src/components/quiz/QuizSplash.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { getTraits } from '../../actions/quizActions';
 import { Redirect } from 'react-router-dom';
+import '../../style/quizSplash.css'
 
 class QuizSplash extends React.Component {
   componentDidMount() {
@@ -15,11 +16,28 @@ class QuizSplash extends React.Component {
           If User is logged in, render this page
           Else redirect to HOME page
         **************************************/}
-        {this.props.token ?
-          <div>
-            <span>QUIZ SPLASH!</span>
-            <span>Welcome {this.props.username}</span>
-            <button onClick={() => this.props.history.push('/quiz')}>Next</button>
+        {localStorage ?
+          <div className="quiz-splash-container">
+            <div className="quiz-top-background">
+            </div>
+
+            <div className="quiz-bottom-background">
+            </div>
+
+            <div className="quiz-overlap-container">
+              <div className="quiz-content-card">
+                <h3>Tell us about yourself</h3>
+                <span></span>
+              </div>
+
+              <div className="quiz-img-icon">
+              </div>
+
+              <div className="quiz-btn">
+                <button onClick={() => this.props.history.push('/quiz')}>Next</button>
+              </div>
+            </div>
+
           </div>
           :
           <Redirect to="/" />
