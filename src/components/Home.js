dd1b3cb07dca1a282ce5bd7a766b307ddeff8fcd
MichaelRomero <liikemike@aim.com>
2019-05-13 18:25:34 -0400

    added screenshot for readme

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,8 @@ class Home extends React.Component {
           </div>
           <div className="paragraph-text">
             <span>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+              On Otaku Meets, you choose your potential matches based on their personality type. No more headaches for catching beautiful people with ugly
+              personalities. Put that fish right back in the ocean and get you another one! Because you deserve better, your future spouse deserves you. So what fun is dating if you can't date with a surprise?
             </span>
           </div>
           <Link to="/signup" className="signup-link">
