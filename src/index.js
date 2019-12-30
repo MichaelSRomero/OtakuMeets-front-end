4d282376dcc66af665766cf835258347a4c1c36e
MichaelRomero <liikemike@aim.com>
2019-12-29 21:36:16 -0500

    moved codebase to a webpack build (not using create-react-app no more)

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import thunk from 'redux-thunk';
 //------------REGULAR IMPORTS-----------//
 import './style/index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import authReducer from './reducers/authReducer'
 import personalityReducer from './reducers/personalityReducer'
 import quizReducer from './reducers/quizReducer'
@@ -32,8 +31,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
