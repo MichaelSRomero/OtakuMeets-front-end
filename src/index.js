829e5b08806cb15e7965e8f0d243e0112d355a4c
MichaelRomero <liikemike@aim.com>
2019-12-31 19:28:13 -0500

    successfully added linter and fixed linting issues, - added rules for linter - fixed a few files (App.js and index.js)

--- src/index.js
+++ src/index.js
@@ -9,19 +9,19 @@ import thunk from 'redux-thunk';
 //------------REGULAR IMPORTS-----------//
 import './style/index.css';
 import App from './App';
-import authReducer from './reducers/authReducer'
-import personalityReducer from './reducers/personalityReducer'
-import quizReducer from './reducers/quizReducer'
-import usersReducer from './reducers/usersReducer'
+import authReducer from './reducers/authReducer';
+import personalityReducer from './reducers/personalityReducer';
+import quizReducer from './reducers/quizReducer';
+import usersReducer from './reducers/usersReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   personalityTracker: personalityReducer,
   quiz: quizReducer,
-  users: usersReducer
-})
+  users: usersReducer,
+});
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -29,5 +29,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
