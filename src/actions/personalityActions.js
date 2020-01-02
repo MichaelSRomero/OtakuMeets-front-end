0b534d84dfb90b6a1f06ad759e1e6b987d4e49e9
MichaelRomero <liikemike@aim.com>
2020-01-01 20:41:18 -0500

    - added PropTypes, - fixed CharacterContainer with linter

--- src/actions/personalityActions.js
+++ src/actions/personalityActions.js
@@ -3,7 +3,7 @@
 /////////////////////////////////////////
 export const addPointToTrait = (trait) => {
   return (dispatch) => {
-    dispatch({type: 'ADD_POINT', payload: trait})
-    return Promise.resolve()
-  }
-}
+    dispatch({ type: 'ADD_POINT', payload: trait });
+    return Promise.resolve();
+  };
+};
--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,40 +1,41 @@
 /////////////////////////////////////////
 //___________ACTION CREATORS___________//
 /////////////////////////////////////////
-const addUsers = (users) => ({type: 'ADD_USERS', payload: users})
-const addMaleUsers = (males) => ({type: 'ADD_MALE_USERS', payload: males})
-const addFemaleUsers = (females) => ({type: 'ADD_FEMALE_USERS', payload: females})
-export const addCurrentUser = (user) => ({type: 'ADD_CURRENT_USER', payload: user})
-export const addCurrentConversation = (conversation) => ({type: 'ADD_CURRENT_CONVERSATION', payload: conversation})
-export const removeCurrentUser = () => ({type: 'REMOVE_CURRENT_USER'})
+const addUsers = (users) => ({ type: 'ADD_USERS', payload: users });
+const addMaleUsers = (males) => ({ type: 'ADD_MALE_USERS', payload: males });
+const addFemaleUsers = (females) => ({ type: 'ADD_FEMALE_USERS', payload: females });
+export const addCurrentUser = (user) => ({ type: 'ADD_CURRENT_USER', payload: user });
+export const addCurrentConversation = (conversation) => ({ type: 'ADD_CURRENT_CONVERSATION', payload: conversation });
+export const removeCurrentUser = () => ({ type: 'REMOVE_CURRENT_USER' });
+
+/////////////////////////////////////////
+//___________HELPER METHODS____________//
+/////////////////////////////////////////
+const filterUsersByGender = (users, gender) => {
+  return users.filter((user) => user.gender.toLowerCase() === gender);
+};
+// Does not include the current user signed in
+const filterOtherUsers = (users, currentUserID) => {
+  return users.filter((user) => user.id !== currentUserID);
+};
+
 /////////////////////////////////////////
 //___________THUNK CREATORS____________//
 /////////////////////////////////////////
 export const getAllUsers = (currentUserID) => {
   return (dispatch) => {
     return fetch(`http://localhost:3000/other/${currentUserID}`)
-      .then(res => res.json())
-      .then(usersJSON => {
+      .then((res) => res.json())
+      .then((usersJSON) => {
         // Pesimistically Update later
-        const otherUsers = filterOtherUsers(usersJSON, currentUserID)
+        const otherUsers = filterOtherUsers(usersJSON, currentUserID);
 
-        const maleUsers = filterUsersByGender(otherUsers, "male")
-        const femaleUsers = filterUsersByGender(otherUsers, "female")
+        const maleUsers = filterUsersByGender(otherUsers, 'male');
+        const femaleUsers = filterUsersByGender(otherUsers, 'female');
 
-        dispatch(addUsers(otherUsers))
-        dispatch(addMaleUsers(maleUsers))
-        dispatch(addFemaleUsers(femaleUsers))
+        dispatch(addUsers(otherUsers));
+        dispatch(addMaleUsers(maleUsers));
+        dispatch(addFemaleUsers(femaleUsers));
       });
-  }
-}
-
-/////////////////////////////////////////
-//___________HELPER METHODS____________//
-/////////////////////////////////////////
-const filterUsersByGender = (users, gender) => {
-  return users.filter(user => user.gender.toLowerCase() === gender);
-}
-// Does not include the current user signed in
-const filterOtherUsers = (users, currentUserID) => {
-  return users.filter(user => user.id !== currentUserID);
-}
+  };
+};
