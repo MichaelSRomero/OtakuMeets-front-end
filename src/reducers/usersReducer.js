fea1015133aa7f2fd15affd5a436845fc7758770
MichaelRomero <liikemike@aim.com>
2020-01-02 01:01:53 -0500

    added linter to all reducers

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -3,30 +3,30 @@ const initialState = {
   maleUsers: [],
   femaleUsers: [],
   currentClicked: {},
-  currentConversation: {}
-}
+  currentConversation: {},
+};
 
 const usersReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case "ADD_USERS":
-      return {...state, allUsers: action.payload};
-    case "ADD_MALE_USERS":
-      return {...state, maleUsers: action.payload};
-    case "ADD_FEMALE_USERS":
-      return {...state, femaleUsers: action.payload}
-    case "ADD_CURRENT_USER":
-      return {...state, currentClicked: action.payload}
-    case "ADD_CURRENT_CONVERSATION":
-      return {...state, currentConversation: action.payload}
-    case "REMOVE_CURRENT_USER":
+  switch (action.type) {
+    case 'ADD_USERS':
+      return { ...state, allUsers: action.payload };
+    case 'ADD_MALE_USERS':
+      return { ...state, maleUsers: action.payload };
+    case 'ADD_FEMALE_USERS':
+      return { ...state, femaleUsers: action.payload };
+    case 'ADD_CURRENT_USER':
+      return { ...state, currentClicked: action.payload };
+    case 'ADD_CURRENT_CONVERSATION':
+      return { ...state, currentConversation: action.payload };
+    case 'REMOVE_CURRENT_USER':
       return {
         ...state,
         currentClicked: {},
         currentConversation: {},
-      }
+      };
     default:
       return state;
   }
-}
+};
 
 export default usersReducer;
