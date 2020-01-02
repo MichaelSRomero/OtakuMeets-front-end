0d3e4108d501e3c1319552649e4ed8271e0720c9
MichaelRomero <liikemike@aim.com>
2020-01-01 21:10:21 -0500

    - added linter to DetailContainer - added linter to Personality - added proptypes to both DetailContainer and Personality

--- src/components/dating/detailFlow/DetailContainer.js
+++ src/components/dating/detailFlow/DetailContainer.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import MatchList from './MatchList';
 import MessageList from './MessageList';
-import { connect } from 'react-redux';
 
 const style = {
-  borderBottom: "rgba(255, 0, 0, 0.8) solid 3px"
-}
+  borderBottom: 'rgba(255, 0, 0, 0.8) solid 3px',
+};
 
-class DetailContainer extends React.Component {
-
+class DetailContainer extends Component {
   state = {
-    currentHeader: "Matches"
+    currentHeader: 'Matches',
   }
 
   changeHeaderOnClick = (e) => {
-    const header = e.target.innerText
-    this.setState({currentHeader: header})
+    const header = e.target.innerText;
+    this.setState({ currentHeader: header });
+  }
+
+  renderHeader = () => {
+    const { currentHeader } = this.state;
+    const {
+      matches,
+      conversations,
+      addCurrentMatchOnClick,
+    } = this.props;
+
+    if (currentHeader === 'Matches') {
+      return (
+        <MatchList
+          matches={matches}
+          addCurrentMatchOnClick={addCurrentMatchOnClick}
+        />
+      );
+    }
+    // Default render
+    return (
+      <MessageList
+        conversations={conversations}
+        addCurrentMatchOnClick={addCurrentMatchOnClick}
+      />
+    );
   }
 
   render() {
+    const { currentHeader } = this.state;
+    const {
+      matches,
+    } = this.props;
 
     return (
       <div className="detail-container">
@@ -26,46 +55,46 @@ class DetailContainer extends React.Component {
         <div className="list-header">
           <div
             className="list-header-text"
-            style={this.state.currentHeader === "Matches" ? style : null}>
+            style={currentHeader === 'Matches' ? style : null}
+          >
             <span onClick={this.changeHeaderOnClick}>Matches</span>
           </div>
 
           <div
             className="list-header-text"
-            style={this.state.currentHeader === "Messages" ? style : null}>
+            style={currentHeader === 'Messages' ? style : null}
+          >
             <span onClick={this.changeHeaderOnClick}>Messages</span>
           </div>
         </div>
         {/* RENDER MESSAGES OR MATCHES BASED ON THE CURRENTHEADER CLICKED */}
         {
-          this.props.matches.length > 0 ?
-          this.state.currentHeader === "Matches" ?
-            <MatchList
-              matches={this.props.matches}
-              addCurrentMatchOnClick={this.props.addCurrentMatchOnClick}/>
-          :
-            <MessageList
-              conversations={this.props.conversations}
-              addCurrentMatchOnClick={this.props.addCurrentMatchOnClick}/>
-          :
-          <div className="detail-list-empty">
-            <div className="empty-img">
-            </div>
-            <p className="empty-title">Get Swiping</p>
-            <p>You have no matches yet</p>
-          </div>
+          matches.length > 0
+            ? this.renderHeader()
+            : (
+              <div className="detail-list-empty">
+                <div className="empty-img" />
+                <p className="empty-title">Get Swiping</p>
+                <p>You have no matches yet</p>
+              </div>
+            )
         }
-
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = ({ auth }) => {
   return {
     matches: auth.matches,
-    conversations: auth.conversations
-  }
-}
+    conversations: auth.conversations,
+  };
+};
+
+DetailContainer.propTypes = {
+  matches: PropTypes.arrayOf(PropTypes.object),
+  conversations: PropTypes.arrayOf(PropTypes.object),
+  addCurrentMatchOnClick: PropTypes.func,
+};
 
 export default connect(mapStateToProps)(DetailContainer);
